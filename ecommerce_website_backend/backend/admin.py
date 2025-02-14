6c2a37103817dfa603318d086c520c8a912add7f
Badris18 <badrivk1008@gmail.com>
2025-02-14 22:47:55 +0530

    update interpreter

--- ecommerce_website_backend/backend/admin.py
+++ ecommerce_website_backend/backend/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')  # Display ID and Name in Admin
+    search_fields = ('name',)  # Enable search by name
+    ordering = ('id',)  # Order by ID
