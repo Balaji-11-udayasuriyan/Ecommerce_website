d3f662766f5a859d3156309b1f659b7f756530c4
Badris18 <badrivk1008@gmail.com>
2025-02-14 22:47:17 +0530

    Update models.py

--- ecommerce_website_backend/backend/models.py
+++ ecommerce_website_backend/backend/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)  # Explicitly defining ID
+    name = models.CharField(max_length=255, unique=True)  # Unique name field
+
+    def __str__(self):
+        return self.name
