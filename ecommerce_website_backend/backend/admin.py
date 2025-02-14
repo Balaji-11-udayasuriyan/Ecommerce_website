10ed1798fce2ea7d070fd0aa8eba4b0472c91891
Badris18 <badrivk1008@gmail.com>
2025-02-14 22:50:57 +0530

    subcategory admin.py updated

--- ecommerce_website_backend/backend/admin.py
+++ ecommerce_website_backend/backend/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, SubCategory
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')  # Display ID and Name in Admin
-    search_fields = ('name',)  # Enable search by name
-    ordering = ('id',)  # Order by ID
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+    ordering = ('id',)
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category')
+    search_fields = ('name', 'category__name')  # Search by subcategory or category name
+    list_filter = ('category',)  # Filter by category
+    ordering = ('id',)
