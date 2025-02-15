c05b7315eda83f2cdf0e59c3ef6ac3b029259f26
Badris18 <badrivk1008@gmail.com>
2025-02-15 09:08:45 +0530

    brand admin added

--- ecommerce_website_backend/backend/admin.py
+++ ecommerce_website_backend/backend/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, SubCategory
+from .models import Category, SubCategory, Brand
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -12,4 +12,10 @@ class SubCategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'category')
     search_fields = ('name', 'category__name')  # Search by subcategory or category name
     list_filter = ('category',)  # Filter by category
+    ordering = ('id',)
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'photo')
+    search_fields = ('name',)
     ordering = ('id',)
